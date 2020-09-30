ce4cb45423c32d66b07a7cd917ac6500657d71e8
Kazuki Hashimoto <kaz205@tuta.io>
2020-09-30 19:36:40 +0900

    build_kernel: Import build script
    Signed-off-by: Kazuki Hashimoto <kaz205@tuta.io>

---
+++ build_kernel.sh
@@ -0,0 +1,17 @@
+export ARCH=arm64
+export SUBARCH=arm64
+export ak=$HOME/AnyKernel3
+export CROSS_COMPILE=$HOME/arm64-gcc/bin/aarch64-elf-
+export CROSS_COMPILE_ARM32=$HOME/arm32-gcc/bin/arm-eabi-
+
+git submodule init && git submodule update
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+make clean -j$(nproc --all) && make mrproper -j$(nproc --all)
+make wahoo_defconfig -j$(nproc --all)
+make -j$(nproc --all) Image.lz4-dtb dtbo.img
+
+cp out/arch/arm64/boot/Image.lz4-dtb $ak/Image.lz4-dtb
+cp out/arch/arm64/boot/dtbo.img $ak/dtbo.img
+cd $ak
+zip -FSr9 $ak/kernel.zip ./*
