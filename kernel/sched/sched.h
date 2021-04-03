f8cdb554ac53fff70cb58e16728adb0bb6d5a4f1
Kazuki Hashimoto <kaz205@tuta.io>
2021-04-03 15:59:09 +0900

    sched: Export task_util
    Signed-off-by: Kazuki Hashimoto <kaz205@tuta.io>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1624,6 +1624,16 @@ extern unsigned int sysctl_sched_use_walt_cpu_util;
 extern unsigned int walt_ravg_window;
 extern bool walt_disabled;
 
+static inline unsigned long task_util(struct task_struct *p)
+{
+#ifdef CONFIG_SCHED_WALT
+	if (likely(!walt_disabled && sysctl_sched_use_walt_task_util))
+		return (p->ravg.demand /
+			(walt_ravg_window >> SCHED_CAPACITY_SHIFT));
+#endif
+	return READ_ONCE(p->se.avg.util_avg);
+}
+
 /**
  * Amount of capacity of a CPU that is (estimated to be) used by CFS tasks
  * @cpu: the CPU to get the utilization of
